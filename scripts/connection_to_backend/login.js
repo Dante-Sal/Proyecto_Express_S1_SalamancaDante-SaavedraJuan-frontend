92de55778a723882020fada8e40957188d61728d
Dante-Sal <dantecampuslands@gmail.com>
2025-09-28 10:25:08 -0500

    fix: :bug: Correcting redirect property path on exclusive pages of anonymity

--- scripts/connection_to_backend/login.js
+++ scripts/connection_to_backend/login.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const meResponse = await fetch(`${API}/users/me`, { credentials: 'include' });
     const meResult = await meResponse.json();
 
-    if (meResponse.status === 200) { window.location.href = window.location.origin + ROOT + meResult.redirect[`${meResult.document.role}`]; return; };
+    if (meResponse.status === 200) { window.location.href = window.location.origin + ROOT + meResult.redirect; return; };
 
     document.getElementById('loginForm').addEventListener('submit', async (e) => {
         e.preventDefault();
