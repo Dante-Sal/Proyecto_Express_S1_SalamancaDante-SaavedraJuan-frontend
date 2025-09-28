17a7247e838d593ee9d8a80ce7fc1d79365c5b6e
Dante-Sal <dantecampuslands@gmail.com>
2025-09-28 10:35:01 -0500

    feat: :lipstick: Adding loading screens to 'login' & 'register' pages

--- scripts/connection_to_backend/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const meResponse = await fetch(`${API}/users/me`, { credentials: 'include' });
-    const meResult = await meResponse.json();
-
-    if (meResponse.status === 200) { window.location.href = window.location.origin + ROOT + meResult.redirect; return; };
-});
